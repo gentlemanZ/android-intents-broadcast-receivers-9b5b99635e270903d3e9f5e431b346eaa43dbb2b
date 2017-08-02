84133963c32475098525d104596b1fc652caefd9
Tianyang Zhong <tianyangzhong2013@gmail.com>
2017-08-02 10:41:48 -0400

    start new activity with explicit intent

--- app/src/main/java/com/teamtreehouse/musicmachine/MainActivity.java
+++ app/src/main/java/com/teamtreehouse/musicmachine/MainActivity.java
@@ -21,7 +21,7 @@ import com.teamtreehouse.musicmachine.models.Song;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
-
+    public static final String EXTRA_TITLE = "EXTRA_TITLE";
     public static final String KEY_SONG = "song";
 
     private boolean mBound = false;
@@ -65,14 +65,8 @@ public class MainActivity extends AppCompatActivity {
         mDownloadButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Downloading", Toast.LENGTH_SHORT).show();
-
-                // Send Messages to Handler for processing
-                for (Song song : Playlist.songs) {
-                    Intent intent = new Intent(MainActivity.this, DownloadIntentService.class);
-                    intent.putExtra(KEY_SONG, song);
-                    startService(intent);
-                }
+                //downloadSongs();
+                testIntents();
             }
         });
         mPlayButton.setOnClickListener(new View.OnClickListener() {
@@ -100,6 +94,24 @@ public class MainActivity extends AppCompatActivity {
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setHasFixedSize(true);
+    }
+
+    private void testIntents() {
+        // Explicit Intent
+        Intent intent = new Intent(this, DetailActivity.class);
+        intent.putExtra(EXTRA_TITLE,"Gradle, Gradle, Gradle");
+        startActivity(intent);
+    }
+
+    private void downloadSongs() {
+        Toast.makeText(MainActivity.this, "Downloading", Toast.LENGTH_SHORT).show();
+
+        // Send Messages to Handler for processing
+        for (Song song : Playlist.songs) {
+            Intent intent = new Intent(MainActivity.this, DownloadIntentService.class);
+            intent.putExtra(KEY_SONG, song);
+            startService(intent);
+        }
     }
 
     public void changePlayButtonText(String text) {
